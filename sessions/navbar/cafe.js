a4a243d4196b04eb2155161ac9203c9daf470329
Rafael da Silva Lima <ryukiytt@gmail.com>
2026-02-05 18:44:33 -0300

    Update event listener to handle multiple card clicks for improved interactivity

--- sessions/navbar/cafe.js
+++ sessions/navbar/cafe.js
@@ -1,4 +1,4 @@
-const btnInfo = document.querySelector(".card");
+const btnInfo = document.querySelectorAll(".card");
 const telaDeInfos = document.querySelector(".telaDeInformacoes");
 
 telaDeInfos.style.visibility = "hidden";
@@ -6,4 +6,7 @@ telaDeInfos.style.visibility = "hidden";
 function informacoes() {
     telaDeInfos.style.visibility = "visible";
 }
-btnInfo.addEventListener("click", informacoes)
+
+btnInfo.forEach(card => {
+    card.addEventListener("click", informacoes);
+});
